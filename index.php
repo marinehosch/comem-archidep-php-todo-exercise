4a6a05bad26ad76b88b6d0eb6ad72977cf8df772
Marine Hosch <marine.hosch@gmail.com>
2022-11-07 11:16:34 +0100

    change db_name default data

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ define('BASE_URL', getenv('TODOLIST_BASE_URL') ?: '/comem-archidep-php-todo-exer
 // Database connection parameters.
 define('DB_USER', getenv('TODOLIST_DB_USER') ?: 'todolistsecond');
 define('DB_PASS', getenv('TODOLIST_DB_PASS'))
-define('DB_NAME', getenv('TODOLIST_DB_NAME') ?: 'todolist');
+define('DB_NAME', getenv('TODOLIST_DB_NAME') ?: '/');
 define('DB_HOST', getenv('TODOLIST_DB_HOST') ?: '127.0.0.1');
 define('DB_PORT', getenv('TODOLSIT_DB_PORT') ?: '3306');
 
